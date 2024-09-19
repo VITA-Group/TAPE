f91c438ef51c270816dc3be059773c87476c69e1
zhuconv <zhuconv@gmail.com>
2024-09-18 21:06:40 -0400

    upload bash script for runining jobs

---
+++ script/long_train.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+nodes=$( scontrol show hostnames $SLURM_JOB_NODELIST )
+nodes_array=($nodes)
+head_node=${nodes_array[0]}
+head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+
+echo Node IP: $head_node_ip
+export LOGLEVEL=INFO
+
+# if [ "$DEBUG" = "true" ]; then
+#   step=1
+#   command="python -m pdb -m torch.distributed.launch"
+# else
+step=1
+command=""
+# fi
+
+# module load anaconda3/2024.6
+# conda activate mantis
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+echo "Running experiment of method $TYPE"
+echo "The path to Python is: $(which python)"
+echo "Command is $command"
+srun torchrun --nproc_per_node $NGPUS --nnodes $NNODES \
+        --rdzv_endpoint $head_node_ip:29500 \
+        --rdzv_id $RANDOM \
+        --rdzv_backend c10d \
+        train_longlora.py  \
+        --ddp_timeout 18000 \
+        --model_name_or_path meta-llama/Llama-2-7b-hf \
+        --resume_from_checkpoint false \
+        --peft_type $TYPE \
+        --bf16 true \
+        --output_dir ./output/llama_$TYPE \
+        --model_max_length 4096 \
+        --use_flash_attn true \
+        --per_device_train_batch_size 1     \
+        --per_device_eval_batch_size 2     \
+        --gradient_accumulation_steps 8     \
+        --evaluation_strategy "no"     \
+        --save_strategy "steps"     \
+        --save_steps 100     \
+        --save_total_limit 1     \
+        --learning_rate 2e-5     \
+        --weight_decay 0.0     \
+        --warmup_steps 50     \
+        --lr_scheduler_type "constant_with_warmup"     \
+        --logging_steps 10     \
+        --deepspeed "config/ds_configs/stage2.json" \
+        --tf32 true \
+        --report_to none \
+        --max_steps 1000
---
+++ script/sptest.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+nodes=$( scontrol show hostnames $SLURM_JOB_NODELIST )
+nodes_array=($nodes)
+head_node=${nodes_array[0]}
+head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+
+echo Node IP: $head_node_ip
+export LOGLEVEL=INFO
+
+if [[ "$SLURM_JOB_NAME" == "interactive" ]]; then
+  step=1
+  command="torchrun"
+else
+  step=1
+  command="srun torchrun"
+fi
+
+# [ -z "${OUTPUT_DIR}" ] && OUTPUT_DIR=./output/${TYPE}_pile  # path to save checkpoints and tensorboard
+# [ -z "${DATA_DIR}" ] && DATA_DIR=  # path to load data
+# [ -z "${CONFIG_NAME}" ] && CONFIG_NAME=config/${TYPE}.json # choose from [config/bipe_rope.json, config/bipe_alibi.json, config/rope.json, config/alibi.json]
+
+for use_flash_attention_2 in flash triton 3triton; do
+  for TYPE in adape; do
+    $command --nproc_per_node $NGPUS --nnodes $NNODES \
+            --rdzv_endpoint $head_node_ip:29512 \
+            --rdzv_id $RANDOM \
+            --rdzv_backend c10d \
+            train.py \
+            --use_flash_attention_2 ${use_flash_attention_2} \
+            --deepspeed "config/ds_configs/stage2.json" \
+            --ddp_timeout 18000 \
+            --dataset_cache_dir ../data/c4 \
+            --output_dir "output/sptest/${TYPE}_${use_flash_attention_2}" \
+            --config_name "config/${TYPE}.json" \
+            --resume_from_checkpoint false \
+            --max_steps 100 \
+            --warmup_steps 10 \
+            --lr_scheduler_type polynomial \
+            --save_steps 10 \
+            --save_total_limit 1 \
+            --eval_steps 10 \
+            --logging_steps 10 \
+            --weight_decay 0.01 \
+            --learning_rate 1e-4 \
+            --model_max_position_embeddings 1024 \
+            --per_device_train_batch_size 16 \
+            --per_device_eval_batch_size 16 \
+            --gradient_accumulation_steps 4 \
+            --do_train True \
+            --do_eval True \
+            --do_predict True \
+            --evaluation_strategy "steps" \
+            --save_strategy "steps" \
+            --load_best_model_at_end True \
+            --report_to "tensorboard" \
+            --gradient_checkpointing False \
+            --bf16 True
+  done
+done
